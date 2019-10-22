2a2114f698f166b33d532790f101ee007f7757fd
Scott Albertine <scott.albertine@gmail.com>
2019-10-22 14:34:22 -0400

    Amazon's nodeJS 10 environment is missing some libs, add them to our zip

--- build_lambda.sh
+++ build_lambda.sh
@@ -15,13 +15,17 @@ echo "-- Updating, downloading and unpacking clamAV and ClamAV update --"
 docker exec -it -w /home/docker s3-antivirus-builder yum install -y cpio yum-utils
 docker exec -it -w /home/docker s3-antivirus-builder yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 docker exec -it -w /home/docker s3-antivirus-builder yum-config-manager --enable epel
-docker exec -it -w /home/docker s3-antivirus-builder yumdownloader -x \*i686 --archlist=x86_64 clamav clamav-lib clamav-update json-c pcre2
+docker exec -it -w /home/docker s3-antivirus-builder yumdownloader -x \*i686 --archlist=x86_64 clamav clamav-lib clamav-update json-c pcre2 libxml2 bzip2-libs libtool-ltdl xz-libs
 docker exec -it -w /home/docker s3-antivirus-builder /bin/sh -c "echo 'folder content' && ls -la"
 docker exec -it -w /home/docker s3-antivirus-builder /bin/sh -c "rpm2cpio clamav-0*.rpm | cpio -idmv"
 docker exec -it -w /home/docker s3-antivirus-builder /bin/sh -c "rpm2cpio clamav-lib*.rpm | cpio -idmv"
 docker exec -it -w /home/docker s3-antivirus-builder /bin/sh -c "rpm2cpio clamav-update*.rpm | cpio -idmv"
 docker exec -it -w /home/docker s3-antivirus-builder /bin/sh -c "rpm2cpio json-c*.rpm | cpio -idmv"
 docker exec -it -w /home/docker s3-antivirus-builder /bin/sh -c "rpm2cpio pcre2*.rpm | cpio -idmv"
+docker exec -it -w /home/docker s3-antivirus-builder /bin/sh -c "rpm2cpio libxml2*.rpm | cpio -idmv"
+docker exec -it -w /home/docker s3-antivirus-builder /bin/sh -c "rpm2cpio bzip2-libs*.rpm | cpio -idmv"
+docker exec -it -w /home/docker s3-antivirus-builder /bin/sh -c "rpm2cpio libtool-ltdl*.rpm | cpio -idmv"
+docker exec -it -w /home/docker s3-antivirus-builder /bin/sh -c "rpm2cpio xz-libs*.rpm | cpio -idmv"
 
 docker stop s3-antivirus-builder
 docker rm s3-antivirus-builder
@@ -35,6 +39,9 @@ echo "-- Cleaning up ClamAV folder --"
 rm -rf clamav
 
 cp -R ./s3-antivirus/* bin/.
+
+rm -r bin/node_modules
+rm -r bin/s3-antivirus.iml
 
 pushd ./bin
 zip -r9 ${LAMBDA_FILE} *
